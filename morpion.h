a4c462bd33bad16140ed5c94556e0f5def6a53ac
root <root@localhost.localdomain>
2018-11-12 14:09:48 +0100

    MrPropre

--- morpion.h
+++ morpion.h
@@ -1,57 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifdef _WIN32
 #define CLRSCR "cls"
 #elif __linux__
 #define CLRSCR "clear"
 #endif
 
-void	name(char *P1, char *P2)
-{	
-	printf("Quel est le nom du joueur 1 (X) ?: ");
-	scanf("%s", P1);
-	printf("Quel est le nom du joueur 2 (O) ?: ");
-	scanf("%s", P2);
-}
+void	name(char *P1, char *P2);
+void	display_tab(char tab[3][3]);
+void	init_tab(char tab[3][3]);
+void	display_head(char *P1, char *P2, int V1, int V2);
+int	check_case(char tab[3][3], int x, int y);
+void	select_case(int x, int y);
 
-void	display_tab(char tab[3][3])
-{
-	printf("\n============================================\n");
-	printf( "\n\n\t\t   0 1 2\n\n"
-		"\t\t0 |%c|%c|%c|\n"
-		"\t\t  -------\n"
-		"\t\t1 |%c|%c|%c|\n"
-		"\t\t  -------\n"
-		"\t\t2 |%c|%c|%c|\n\n\n", tab[0][0], tab[0][1], tab[0][2], tab[1][0], tab[1][1], tab[1][2], tab[2][0], tab[2][1], tab[2][2]);
-	printf("\n============================================\n\n");
-}
-
-void	init_tab(char tab[3][3])
-{
-	for (int x = 0; x < 3; x++)
-		for (int y = 0; y < 3; y++)
-			tab[x][y] = ' ';
-}
-
-void	display_head(char *P1, char *P2, int V1, int V2)
-{
-	printf(	"Partie en cours: %s VS %s.\n\n"
-		"Score: \n"
-		"%s = %d.\n"
-		"%s = %d.\n", P1, P2, P1, V1, P2, V2);
-}
-
-int	check_case(char tab[3][3], int x, int y)
-{
-	if (tab[x][y] != ' ')
-		return 1;
-	if ((x < 0 || x > 2) && (y < 0 || y > 2))
-		return 2;
-	return 0;
-}
-
-void	select_case(int x, int y)
-{
-	printf("ligne: ");
-	scanf("%d", &x);
-	printf("Colone: ");
-	scanf("%d", &y);
-}
